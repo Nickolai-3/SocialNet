a51704ef70ac164b7089d493c06fde610d9d2d14
Nick <47428427+Nickolai-3@users.noreply.github.com>
2019-08-28 20:27:01 +0300

    111

---
+++ src/components/Navbar.js
@@ -0,0 +1,25 @@
+import React from 'react';
+
+
+const Navbar = () => {
+    return (<nav className='nav'>
+        <div>
+            <div>
+                <a>Profile</a>
+            </div>
+            <div>
+                <a>Messenger</a>
+            </div>
+            <div>
+                <a>News</a>
+            </div>
+            <div>
+                <a>Music</a>
+            </div><div>
+                <a>Settings</a>
+            </div>
+        </div>
+    </nav>
+    )
+}
+export default Navbar;
---
+++ src/components/Profile.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+
+const Profile = () => {
+  return (<div className='content'>
+    <img src='https://helpx.adobe.com/content/dam/help/en/stock/how-to/visual-reverse-image-search/jcr_content/main-pars/image/visual-reverse-image-search-v2_intro.jpg' />
+    <div> <img1 src='http://webresizer.com/images2/bird1_before.jpg'></img1>
+    </div>
+    <div>Flsj
+  </div>
+    <div>лоло Post
+
+  </div>
+    <div>Post11</div>
+    <div>Post2</div>
+  </div>
+  )
+}
+export default Profile;
